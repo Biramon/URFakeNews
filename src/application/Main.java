16085a6a4965b4b4b8caaf0de671171fb51d8b3a
ubirajarajunior <ubirajarajunior@SINFO-0683-PC.ufrn.local>
2019-06-04 17:29:56 -0300

    Adicionei a tela principal. Em casa continuo a altera-la.

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,15 +9,7 @@ import javafx.scene.layout.BorderPane;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
 	public static void main(String[] args) {
