00a624ff52d5f0d8e473d78073e666c10cd54a6a
Ubirajara Junior <ubirajara.viegas@yahoo.com.br>
2019-06-06 00:10:38 -0300

    mudanças no controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,22 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws IOException {
+		Pane root = FXMLLoader.load( getClass().getResource("TelaPrincipal.fxml") );
 		
+		Scene cena = new Scene(root, 600, 400); 
+		primaryStage.setScene(cena);
+		primaryStage.show(); 
 	}
 	
 	public static void main(String[] args) {
